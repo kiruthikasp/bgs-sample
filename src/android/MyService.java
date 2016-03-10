99b337a60fecb1e9d84699b0b7aad987f03dad7f
kiruthikasp <kiruthikait93@gmail.com>
2016-03-10 10:31:56 +0530

    Update MyService.java

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -14,7 +14,8 @@ import android.util.Log;
 import android.widget.Toast;
 import android.os.Binder;
 import android.os.IBinder;
-
+import android.os.Handler;
+import android.content.Context;
 import android.content.Intent;
 
 import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
@@ -25,47 +26,47 @@ public class MyService extends BackgroundService {
 	
 	private String mHelloTo = "World";
 	private static Timer timer = new Timer(); 
-    private Context ctx;
-
-    public IBinder onBind(Intent arg0) 
-    {
-          return null;
-    }
-
-    public void onCreate() 
-    {
-          super.onCreate();
-          ctx = this; 
-          startService();
-    }
-
-    private void startService()
-    {           
-        timer.scheduleAtFixedRate(new mainTask(), 0, 5000);
-    }
-
-    private class mainTask extends TimerTask
-    { 
-        public void run() 
-        {
-            toastHandler.sendEmptyMessage(0);
-        }
-    }    
-
-    public void onDestroy() 
-    {
-          super.onDestroy();
-          Toast.makeText(this, "Service Stopped ...", Toast.LENGTH_SHORT).show();
-    }
-
-    private final Handler toastHandler = new Handler()
-    {
-        @Override
-        public void handleMessage(Message msg)
-        {
-            Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
-        }
-    };    
+	    private Context ctx;
+	
+	    public IBinder onBind(Intent arg0) 
+	    {
+	          return null;
+	    }
+	
+	    public void onCreate() 
+	    {
+	          super.onCreate();
+	          ctx = this; 
+	          startService();
+	    }
+	
+	    private void startService()
+	    {           
+	        timer.scheduleAtFixedRate(new mainTask(), 0, 5000);
+	    }
+	
+	    private class mainTask extends TimerTask
+	    { 
+	        public void run() 
+	        {
+	            toastHandler.sendEmptyMessage(0);
+	        }
+	    }    
+	
+	    public void onDestroy() 
+	    {
+	          super.onDestroy();
+	          Toast.makeText(this, "Service Stopped ...", Toast.LENGTH_SHORT).show();
+	    }
+	
+	    private final Handler toastHandler = new Handler()
+	    {
+	        @Override
+	        public void handleMessage(Message msg)
+	        {
+	            Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+	        }
+	    };    
 	@Override
 	protected JSONObject doWork() {
 		JSONObject result = new JSONObject();
