9af935762437d495370246e5ad0c1529dd1144f3
kiruthikasp <kiruthikait93@gmail.com>
2016-04-02 16:18:00 +0530

    Update MyService.java

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -59,23 +59,48 @@ public class MyService extends BackgroundService {
 	
 	    private class mainTask extends TimerTask
 	    { 
+	    	    private final static long ONCE_PER_DAY = 1000*60*60*24;
+
+		    //private final static int ONE_DAY = 1;
+		    private final static int TWO_AM = 9;
+		    private final static int ZERO_MINUTES = 0;
 	        public void run() 
 	        {
+	        	long currennTime = System.currentTimeMillis();
+	        	long stopTime = currennTime + 2000;//provide the 2hrs time it should execute 1000*60*60*2
+		          while(stopTime != System.currentTimeMillis()){
+		              // Do your Job Here
+		            System.out.println("Start Job"+stopTime);
+		            System.out.println("End Job"+System.currentTimeMillis());
+		          }
         	   start();		
                    toastHandler.sendEmptyMessage(0);
                    
 	        }
 	    }    
-	
+	private static Date getTomorrowMorning2AM(){
+
+        Date date2am = new java.util.Date(); 
+           date2am.setHours(TWO_AM); 
+           date2am.setMinutes(ZERO_MINUTES); 
+
+           return date2am;
+      }
+    //call this method from your servlet init method
+    public static void startTask(){
+        MyTimerTask task = new MyTimerTask();
+        Timer timer = new Timer();  
+        timer.schedule(task,getTomorrowMorning2AM(),1000*10);// for your case u need to give 1000*60*60*24
+    }
+    public static void main(String args[]){
+        start();		
+        toastHandler.sendEmptyMessage(0);
+
+    }
+    
 	public void start() {
 		
 		Intent LaunchIntent;
-		
-		String com_name = null;
-		String activity = null;
-
-		com_name = "com.pinnacle.hr";
-		activity = "com.pinnacle.hr.MAIN";
 		
 		try {
 		
