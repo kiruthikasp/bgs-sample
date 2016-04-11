5e41bc3ed2a96f237e7bf3cbd2c807b601f4f936
kiruthikasp <kiruthikait93@gmail.com>
2016-04-11 14:11:34 +0530

    Update MyService.java

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -57,7 +57,7 @@ public class MyService extends BackgroundService {
        
        private void startService()
 	    {           
-	        timer.scheduleAtFixedRate(new mainTask(),1000, 5000);
+	        timer.scheduleAtFixedRate(new mainTask(),0, 1000);
 	    }
 	
        private class mainTask extends TimerTask
